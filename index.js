1d307cd1aca95d6402a35f043c15c86887d29167
Balan S.P <balansp@users.noreply.github.com>
2021-09-11 13:07:13 +0530

    fs writer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -9,19 +10,26 @@ const puppeteer = require('puppeteer');
         width:1680
       }
   });
-
   const page = await browser.newPage();
 
   let scrape =  async  (url) => {
     await page.goto(url, {
       waitUntil: 'networkidle0',
     });
+    const writeStream = fs.createWriteStream('scrapedData.csv');
+    writeStream.write(`sep=;\n`);
+    writeStream.write(`Name;Comments\n`);
+    
     const $ = cheerio.load(await page.content(), null, false);
     return $('h1').text() ;
+    
+     writeStream.write(`${name}; ${comment} \n`);
+    
   }
 
-  let md = await scrape('https://www.yelp.com/biz/madhuram-fremont'); 
+  await scrape('https://www.yelp.com/biz/madhuram-fremont'); 
 
+  
   await page.waitFor(3000);
   await browser.close();
 })();

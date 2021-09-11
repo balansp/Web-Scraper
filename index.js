dc306a97763aae7e6f44504e4d22deca1b155f57
balansp <balan.spx@gmail.com>
2021-09-11 14:18:51 +0530

    updated

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ const DEL = ';';
   });
   const page = await browser.newPage();
 
+  const writeStream = fs.createWriteStream('scrapedData.csv');
+  writeStream.write(`sep=;\n`);
+  writeStream.write(`Name;Rating;Comments\n`);
+
   let scrape =  async  (url) => {
     await page.goto(url, {
       waitUntil: 'networkidle0',
     });
-    const writeStream = fs.createWriteStream('scrapedData.csv');
-    writeStream.write(`sep=;\n`);
-    writeStream.write(`Name;Comments\n`);
+    
     
     const $ = cheerio.load(await page.content(), null, false);
      $('h1').text() ;
@@ -29,25 +31,45 @@ const DEL = ';';
     const reviewList= 'ul.undefined.list__373c0__vNxqp li ';
 
     $(reviewList).each((i, el) => {
-
-      const userName = $(el)
-        .find('.fs-block a.css-166la90')
+      let scrapeDataArr =[],  cnt=0;
+      let scrapeDataStr = '';
+      //UserName
+       scrapeDataArr[cnt++] = $(el)
+        .find('.fs-block a.css-166la90') 
         .text();
-      const comment = $(el)
-        .find('p')
-        .text()
-        .replace(/\s\s+/g, '');
-      
+
+      //Ratings
+
+      scrapeDataArr[cnt++] = $(el)
+          .find('.i-stars__373c0___sZu0')
+          .attr('aria-label')
+          
+
+      //Number of Review
+
+      //Number of friends
+
+
+      //Comments
+      scrapeDataArr[cnt++] = $(el)
+      .find('p')
+      .text()
+      .replace(/\s\s+/g, '');
+
+
+      scrapeDataStr =  scrapeDataArr.join(DEL)
+
       // Write Row To CSV
-      writeStream.write(`${userName} ${DEL} ${comment} ${DEL} \n`);
+      writeStream.write(`${scrapeDataStr}\n`);
     });
 
      //writeStream.write(`${name}; ${comment} \n`);
   
   };
 
-  for(let i=0;i<=30;i+10){
-    await scrape('https://www.yelp.com/biz/madhuram-fremont?start=${i}'); 
+  for(let i=0;i<=30;i=i+10){
+    console.log(`https://www.yelp.com/biz/madhuram-fremont?start=${i}`)
+    await scrape(`https://www.yelp.com/biz/madhuram-fremont?start=${i}`); 
   }
   
 

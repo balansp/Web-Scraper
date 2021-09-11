752eba3c34e8cdb2135ab41fdff3eda52deff1c2
Balan <balan.sp@sap.com>
2021-09-11 22:51:07 +0530

    Amenities Implemnted

--- index.js
+++ index.js
@@ -17,44 +17,92 @@ const DEL = ';';
 
   const writeStream = fs.createWriteStream('scrapedData.csv');
   writeStream.write(`sep=;\n`);
-  writeStream.write(`Name;Rating;Comments\n`);
 
-  let scrape =  async  (url) => {
+  let column=[];
+  column.push('PageNo');
+  column.push('Name');
+  column.push('Date');
+  column.push('Rating');
+  column.push('no of Reviews');
+  column.push('no of Friends');
+  column.push('no of Photos');
+  column.push('Amenities');
+  column.push('Comments');
+
+  let strColumn = column.join(DEL);
+
+  writeStream.write(`${strColumn}\n`);
+
+  let scrape =  async  (url,pageNo) => {
+    pageNo = pageNo/10 +1; 
     await page.goto(url, {
       waitUntil: 'networkidle0',
     });
     
+    await page.click('[aria-label="Amenities and More"] button');
+
     
     const $ = cheerio.load(await page.content(), null, false);
+
+
+    let arrAmenities=[];
+    let strAmenities="";
+     
+   $('[aria-label="Amenities and More"] .gutter-2__373c0__1fwxZ  .arrange-unit__373c0__2u2cR.arrange-unit-fill__373c0__3cIO5.border-color--default__373c0__2s5dW').each((i, el) => {
+        arrAmenities.push($(el).find('.css-1h1j0y3').text());
+    });
+
+    strAmenities = arrAmenities.join(', ')
+
      $('h1').text() ;
     
-    const reviewList= 'ul.undefined.list__373c0__vNxqp li ';
 
-    $(reviewList).each((i, el) => {
+    $('ul.undefined.list__373c0__vNxqp li').each((i, el) => {
       let scrapeDataArr =[],  cnt=0;
       let scrapeDataStr = '';
+
+       //UserName
+       scrapeDataArr[cnt++] = pageNo;
+
       //UserName
        scrapeDataArr[cnt++] = $(el)
         .find('.fs-block a.css-166la90') 
         .text();
 
-      //Ratings
 
+       //Date
+     scrapeDataArr[cnt++] = $(el)
+        .find('.gutter-1__373c0__3zF2l .css-e81eai')
+        .text();
+
+
+      //Ratings
       scrapeDataArr[cnt++] = $(el)
           .find('.i-stars__373c0___sZu0')
           .attr('aria-label')
           
-
       //Number of Review
+      scrapeDataArr[cnt++] = $(el)
+         .find('.user-passport-stats__373c0__2nBtY [aria-label="Reviews"] .css-1dgkz3l')
+         .text()
 
-      //Number of friends
+      //Number of Friends 
+      scrapeDataArr[cnt++] = $(el)
+         .find('.user-passport-stats__373c0__2nBtY [aria-label="Friends"] .css-1dgkz3l')
+         .text()
 
+      //Number of Photos 
+      scrapeDataArr[cnt++] = $(el)
+        .find('.user-passport-stats__373c0__2nBtY [aria-label="Photos"] .css-1dgkz3l')
+        .text()
+
+        scrapeDataArr[cnt++] =  strAmenities;
 
       //Comments
       scrapeDataArr[cnt++] = $(el)
-      .find('p')
-      .text()
-      .replace(/\s\s+/g, '');
+        .find('p')
+        .text()
+        .replace(/\s\s+/g, '');
 
 
       scrapeDataStr =  scrapeDataArr.join(DEL)
@@ -67,13 +115,12 @@ const DEL = ';';
   
   };
 
-  for(let i=0;i<=30;i=i+10){
-    console.log(`https://www.yelp.com/biz/madhuram-fremont?start=${i}`)
-    await scrape(`https://www.yelp.com/biz/madhuram-fremont?start=${i}`); 
+  for(let i=0;i<=10;i=i+10){
+    await scrape(`https://www.yelp.com/biz/madhuram-fremont?start=${i}`,i); 
   }
   
 
   writeStream.close();
-  await page.waitFor(1000);
+  //await page.waitFor(1000);
   await browser.close();
 })();

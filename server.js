64925014b2002ebabe8eff676946e27df3290e1b
Balan <balan.sp@sap.com>
2021-09-26 21:09:40 +0530

    Front End UI Implemented

--- server.js
+++ server.js
@@ -1,34 +1,54 @@
 const express = require("express");
-const app = express();
+const bodyParser = require('body-parser');
 const path = require("path");
-const router = express.Router();
 
-const APP_NAME ="Motif - Web Scrapper"
+const userData = require("./users");
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const server = http.createServer();
+server.listen(9898);
+const wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+let  connection ;
+wsServer.on('request', function(request) {
+   connection = request.accept(null, request.origin);
+   connection.on('message', function(message) {
+      console.log('Received Message:', message.utf8Data);
+      connection.sendUTF('Ready!');
+  });
+  connection.on('close', function(reasonCode, description) {
+      console.log('Client has disconnected.');
+  });
+});
+
+const app = express();
+const router = express.Router();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const port = 3000;
+
+const APP_NAME = "Motif - Web Scrapper";
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-
-
 router.get("/", (req, res) => {
-  res.render(__dirname+"/ui/index", { title: APP_NAME, message: "Hello there!" });
+  res.render(__dirname+"/ui/index", { title: APP_NAME, message: "Data Scrapper!" });
 });
 
-// router.post("/scrapper", (req, res) => {
-//     res.render(__dirname+"/ui/index", { title: APP_NAME, message: "Hello there!" });
-// });
 
-app.post("/scrapper", (req, res) =>{
-    // var num1 = Number(req.body.num1);
-    // var num2 = Number(req.body.num2);
-     
-   // var result = num1 + num2 ;
-     console.log(req)
-    res.send("Addition - " + JSON.stringify );
-  });
+
+app.post('/scraper', function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  userData.scraper(request.body.json,connection);
+  response.end('ok')
+})
 
 
 app.use("/", router);
-app.listen(process.env.port || 3000);
-
-console.log("Running at Port 3000");
+app.listen(process.env.port || port);
+console.log(`Running at Port ${port}`);

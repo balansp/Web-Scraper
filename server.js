69c26d4177b61ede12a4aa58533eddd12b350b56
Balan <balan.sp@sap.com>
2021-09-30 01:08:34 +0530

    Authentication Implemneted

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const path = require("path");
-const fs = require('fs');
-const userData = require("./users");
-const http = require('http');
-const WebSocketServer = require('websocket').server;
-const server = http.createServer();
-server.listen(3001);
-const wsServer = new WebSocketServer({
-  httpServer: server
-});
+const express = require("express"),
+      session = require('express-session')
+      bodyParser = require('body-parser'),
+      path = require("path"),
+      fs = require('fs'),
+      userData = require("./users"),
+      http = require('http'),
+      WebSocketServer = require('websocket').server,
+      server = http.createServer(),
+      wsServer = new WebSocketServer({
+        httpServer: server
+      });
+const app = express();
+const router = express.Router();
+
+const APP_NAME = "Motif - Web Scrapper";
+const port = 3000;
 
 let  connection ;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: 'Keep it secret',
+  name:'uniqueSessionID',
+  cookie: { maxAge: 1000 * 60 * 60 * 24 }, //maxAge = 1 day
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", router);
+app.listen(process.env.port || port);
+server.listen(port +1 );
+
+console.log(`Running @ ${port}`);
+console.log(`Socket @ ${port+1}`);
+
+
 wsServer.on('request', function(request) {
    connection = request.accept(null, request.origin);
    connection.on('message', function(message) {
@@ -23,45 +52,49 @@ wsServer.on('request', function(request) {
   });
 });
 
-const app = express();
-const router = express.Router();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+let isSessionValid = (req) => (req.session && req.session.email && req.session.email=='balan.spx@gmail.com');
 
-const port = 3000;
-
-const APP_NAME = "Motif - Web Scrapper";
-
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-
+//************************************************************ Routers ************************************************************
 router.get("/", (req, res) => {
-  res.render(__dirname+"/ui/index", { title: APP_NAME, message: "Data Scrapper!" });
+  if(isSessionValid(req)){
+    res.render(__dirname+"/ui/index", { title: APP_NAME, email:req.session.email }); 
+  } else {  
+    res.render(__dirname+"/ui/login", { title: APP_NAME, message: "Data Scrapper!" });
+  }
+  
 });
 
 router.get("/csvfiles", (req, res) => {
-    let filesJson={};
-
-    res.writeHead(200, {"Content-Type": "application/json"});
-      const directoryPath = path.join(__dirname, 'output');
-      fs.readdir(directoryPath, function (err, files) {
-          if (err) {
-              return console.log('Unable to scan directory: ' + err);
-          } 
-          filesJson.files = files
-          res.write(JSON.stringify(filesJson));
-          res.end('');
-      });
+  if(!isSessionValid(req)){
+    res.sendStatus(401);
+    return;
+  }
+  let filesJson={};
+  res.writeHead(200, {"Content-Type": "application/json"});
+    const directoryPath = path.join(__dirname, 'output');
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        filesJson.files = files
+        res.write(JSON.stringify(filesJson));
+        res.end('');
+    });
 });
 
 router.get("/download", (req, res) => {
-  console.log('Downloading '+ req.query.file);
+  if(!isSessionValid(req)){
+    res.sendStatus(401);
+    return;
+  }
   res.download("output/" + req.query.file)
 });
 
 router.get("/clearDir", (req, res) => {
-
+  if(!isSessionValid(req)){
+    res.sendStatus(401);
+    return;
+  }
   fs.readdir('output', (err, files) => {
     if (err) throw err;
     for (const file of files) {
@@ -74,11 +107,22 @@ router.get("/clearDir", (req, res) => {
 });
 
 app.post('/scraper', function(request, response) {
+  if(!isSessionValid(req)){
+    res.sendStatus(401);
+    return;
+  }
   response.writeHead(200, {'Content-Type': 'text/html'})
   userData.scraper(request.body.json,connection);
   response.end('ok')
 })
 
-app.use("/", router);
-app.listen(process.env.port || port);
-console.log(`Running at Port ${port}`);
+
+app.post('/',bodyParser.urlencoded(),(req,res)=>{
+    if(req.body.email=='balan.spx@gmail.com' && req.body.password=='open') {
+       req.session.email=req.body.email;
+       console.log("Login Success!!!");
+        res.render(__dirname+"/ui/index", { title: APP_NAME,email:req.session.email }); 
+    }  else {
+       res.sendStatus(401) ;
+    }
+});
